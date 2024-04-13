7021cb337f253391c20a6580c1ed1bc8ea7c8dfe
PedroFDiniz <63296960+PedroFDiniz@users.noreply.github.com>
2024-04-12 22:40:49 -0300

    doc(user.service): add documentation to the functions. doc(task.service): add documentation to the functions.

--- src/service/task.service.ts
+++ src/service/task.service.ts
@@ -6,6 +6,14 @@ import { Log } from "../util/log";
  * Descreve todas as interações do modelo "Task".
  */
 class TaskService {
+    /**
+     * Cria uma nova tarefa. O valor base do status é 'PENDENTE'.
+     * @param { string? } name O nome a tarefa. Opcional.
+     * @param { string } date A data da tarefa. Obrigatória.
+     * @param { string? } description A descrição da tarefa. Opcional.
+     * @returns Um objeto Tarefa, como descrito no esquema mongoose em
+     * 'src/model/task.ts'.
+     */
     async create(name:string, date:string, description:string) {
         try {
             return await Task.create({
@@ -17,6 +25,11 @@ class TaskService {
         } catch (error:any) { return null; }
     }
 
+    /**
+     * Altera o campo 'status' de uma tarefa para 'CONCLUIDA'.
+     * @param { string } id A tarefa a ser alterada.
+     * @returns 
+     */
     async setComplete(id:string) {
         try {
             const task = await Task.findById(id);
@@ -26,11 +39,21 @@ class TaskService {
         } catch (error:any) { return null; }
     }
 
+    /**
+     * Busca uma tarefa.
+     * @param { string } id O ID da tarefa a ser buscada.
+     * @returns Um objeto Task.
+     */
     async fetch(id:string) {
         try { return await Task.findById(id); }
         catch (error:any) { return null; }
     }
 
+    /**
+     * Busca várias tarefas.
+     * @param { string[] } ids Os IDs das tarefas a serem buscadas.
+     * @returns Uma lista de objetos Task.
+     */
     async fetchMany(ids:mongoose.Types.ObjectId[]) {
         try {
             const tasks = await Task.find({
@@ -40,6 +63,11 @@ class TaskService {
         } catch (error:any) { return null; }
     }
 
+    /**
+     * Apaga tarefas do banco de dados.
+     * @param { string[] } ids Os IDs das tarefas a serem apagadas.
+     * @returns 
+     */
     async delete(ids:string[]) {
         try { return await Task.deleteMany({ '_id': { $in: ids }}); }
         catch (error:any) { return null; }
--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -7,6 +7,17 @@ import { service as tasks } from "./task.service";
  * Descreve todas as interações do modelo "User".
  */
 class UserService {
+    /**
+     * Cria um novo usuário.
+     * @param { string } name O nome completo do usuário.
+     * @param { number } age A idade do usuário.
+     * @param { string } sex O sexo do usuário. Aceita apenas
+     * valores "M" ou "F".
+     * @param email Email do usuário. Será usado para login.
+     * @param password Senha do usuário.
+     * @returns Um documento User direto do banco de dados, como descrito no
+     * esquema mongoose em 'src/model/user.ts'.
+     */
     async create(
         name:string,
         age:number,
@@ -24,11 +35,16 @@ class UserService {
             });
             return newUser;
         } catch (error:any) {
-            Log.write(`Error: ${error.name}`);
             return null;
         }
     }
 
+    /**
+     * Resgata um usuário do banco de dados.
+     * @param { string } id O id do usuário a ser resgatado.
+     * @returns Retorna um objeto usuário como descrito no esquema mongoose
+     * em 'src/model/user.ts'.
+     */
     async get(id:string) {
         if (!id || id === "") return null;
         const user =
@@ -45,6 +61,12 @@ class UserService {
         }
     }
 
+    /**
+     * Resgata todas as tarefas pertencentes a um usuário.
+     * @param { string } id O id do usuário cujas tarefas devem ser resgatadas.
+     * @returns Uma lista contendo objetos Task, como descritos no esquema
+     * mongoose em 'src/model/task.ts'.
+     */
     async getTasks(id:string) {
         const user = await User.findById(id, "_id tasks");
         if (!user) return null;
@@ -53,6 +75,11 @@ class UserService {
         return taskList;
     }
 
+    /**
+     * Apaga tarefas da lista de um usuário e do banco de dados.
+     * @param { string } userId O usuário cujas tarefas serão apagadas.
+     * @param { string[] } ids Os ids das tarefas a serem apagadas.
+     */
     async deteleTasks(userId:string, ids:string[]) {
         for (let id in ids) {
             const updatedUser = await User.updateOne({
@@ -66,22 +93,50 @@ class UserService {
         tasks.delete(ids);
     }
 
+    /**
+     * Busca apenas os quatro campos relevantes para o login de um usuário.
+     * @param { string } email O endereço de email do usuário.
+     * @returns Retorna um objeto User, mas apenas com os campos '_id',
+     * 'email', 'password' e 'token'.
+     */
     async fetchLogin(email:string) {
         return await User.findOne({ email }, "_id email password token");
     }
 
+    /**
+     * Encontra um usuário pelo seu ID.
+     * @param { string } id O id do usuário a ser resgatado
+     * @returns Um objeto usuário.
+     */
     async findById(id:string) {
         if (id && id !== "")
             return await User.findById( id );
         return null;
     }
 
+    /**
+     * Resgata um usuário do banco de dados a partir de uma das 3 opções de
+     * campos: id, nome ou email. A pesquisa é executada sequencialmente nesta
+     * exata ordem, e apenas resgata o primeiro que for encontrado.
+     * @param { string? } id O id do usuário em questão.
+     * @param { string? } name O nome do usuário em questão.
+     * @param { string? } email O email do usuário em questão.
+     * @returns Um objeto usuário.
+     */
     async findUser(id:string, name:string, email:string) {
         if (id)     return await User.findById( id );
         if (name)   return await User.find({ name: name });
         if (email)  return await User.find({ email: email });
     }
 
+    /**
+     * Edita informações de um usuário.
+     * @param { string } id O id do usuário a ser alterado
+     * @param { string? } name O novo nome do usuário. Opcional.
+     * @param { number? } age A nova idade do usuário. Opcional.
+     * @param { string? } picture A nova imagem do usuário. Opcional.
+     * @returns O objeto User, já atualizado.
+     */
     async edit(
         id:string,
         name:string = "",
@@ -98,11 +153,21 @@ class UserService {
         return await User.findOneAndUpdate(filter, update);
     }
 
+    /**
+     * Resgata todos os usuários.
+     * @returns Uma lista contendo todos os usuários presentes no banco.
+     */
     async findAll() {
         let result = await User.find({ });
         return result;
     }
 
+    /**
+     * Adiciona uma tarefa à lista do usuário
+     * @param { string } userId O ID do usuário a receber a tarefa.
+     * @param { string } taskId O ID da tarefa a ser adicionada.
+     * @returns O objeto usuário, já atualizado.
+     */
     async addTask(userId: any, taskId: any) {
         const filter = { _id: userId };
         const update = { $push: { tasks: taskId } };
